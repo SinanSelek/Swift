289c5dbd0bdbe0b8a0106d345097c37b7215826c
Swift <sselek62@gmail.com>
2022-06-30 22:25:26 +0300

    Degiskenler Calismasi

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+//Variables & Constants
+
+//String
+
+var userName = "Sinan"
+userName.append("s")
+userName.lowercased()
+userName.uppercased()
+
+var userSurname = "Selek"
+
+userName = "Lars"
+
+//Integer
+
+let userAge = 50
+let myNumber = 4
+userAge / myNumber
+
+//Double
+
+let userAgeD = 50.0
+let myNumberD = 4.0
+userAgeD / myNumberD
+
+//Boolean
+
+var myBoolean = false
+myBoolean = true
+
+
+
